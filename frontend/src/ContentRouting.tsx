a932fb89c42c7b8b2153788ee8118b0d68b07a18
Michal Cikatricis <xcikatr@mail.muni.cz>
2021-06-21 17:35:58 +0200

    protected routing

--- frontend/src/ContentRouting.tsx
+++ frontend/src/ContentRouting.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import ProtectedRoute from "./auth/ProtectedRoute";
 import Profile from "./common/Profile";
 import TestCourses from "./common/TestCourses";
 import CourseDetail from "./courses/CourseDetail";
@@ -8,34 +9,43 @@ import CreateCourse from "./courses/CreateCourse";
 import EditCourse from "./courses/EditCourse";
 import MyCourses from "./courses/MyCourses";
 import Home from "./Home";
+import NotFound from "./status/NotFound";
+import Unauthorized from "./status/Unauthorized";
 
 function ContentRouting() {
+
     return (
         <div className="site-layout-content">
             <Switch>
-                <Route path="/mycourses">
+                <ProtectedRoute path="/mycourses">
                     <MyCourses />
-                </Route>
-                <Route path="/courses">
+                </ProtectedRoute>
+                <ProtectedRoute path="/courses">
                     <Courses />
-                </Route>
-                <Route path="/test-courses">
+                </ProtectedRoute>
+                <ProtectedRoute path="/test-courses">
                     <TestCourses />
-                </Route>
-                <Route path="/profile">
+                </ProtectedRoute>
+                <ProtectedRoute path="/profile">
                     <Profile />
-                </Route>
-                <Route path="/course/new">
+                </ProtectedRoute>
+                <ProtectedRoute path="/course/new">
                     <CreateCourse />
-                </Route>
-                <Route path="/course/:id/edit">
+                </ProtectedRoute>
+                <ProtectedRoute path="/course/:id/edit">
                     <EditCourse />
-                </Route>
-                <Route path="/course/:id">
+                </ProtectedRoute>
+                <ProtectedRoute path="/course/:id">
                     <CourseDetail />
-                </Route>
-                <Route path="/">
+                </ProtectedRoute>
+                <Route exact path="/">
                     <Home />
+                </Route>
+                <Route exact path="/unauthorized">
+                    <Unauthorized />
+                </Route>
+                <Route>
+                    <NotFound />
                 </Route>
             </Switch>
         </div>
