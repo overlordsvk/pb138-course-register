18c1831e763e05a3e279bbe5b441e8f91a20fa49
Marius Molcany <molcanym@gmail.com>
2021-06-22 18:56:42 +0200

    WIP: protected routes with roles

--- frontend/src/menu/MainMenu.tsx
+++ frontend/src/menu/MainMenu.tsx
@@ -1,13 +1,52 @@
 import React from "react";
 import { Menu } from "antd";
-import { SolutionOutlined, UnorderedListOutlined } from "@ant-design/icons";
+import { CalendarOutlined, RobotOutlined, SolutionOutlined, UnorderedListOutlined, UserOutlined } from "@ant-design/icons";
 import "./MainMenu.css";
 import { Link, useLocation } from "react-router-dom";
 import AuthenticationButton from "../auth/AuthenticationButton";
 import { useAuth0 } from "@auth0/auth0-react";
+import { isAdmin } from "../utils/helpers";
+
 
 function MainMenu() {
     const { isAuthenticated } = useAuth0();
+    var menu = <></>;
+    if (isAuthenticated) {
+        menu = <>
+            <Menu.Item
+                className="menu-item"
+                key="/mycourses"
+                icon={<SolutionOutlined />}
+            >
+                <Link to="/mycourses">My courses</Link>
+            </Menu.Item>
+            <Menu.Item
+                className="menu-item"
+                key="/courses"
+                icon={<UnorderedListOutlined />}
+            >
+                <Link to="/courses">Course list</Link>
+            </Menu.Item>
+        </>;
+    }
+    if (isAdmin()) {
+        menu = <>
+            <Menu.Item
+                className="menu-item"
+                key="/users"
+                icon={<UserOutlined />}
+            >
+                <Link to="/users">Users</Link>
+            </Menu.Item>
+            <Menu.Item
+                className="menu-item"
+                key="/semesters"
+                icon={<CalendarOutlined />}
+            >
+                <Link to="/semester">Semesters</Link>
+            </Menu.Item>
+        </>;
+    }
     return (
         <div>
             <Menu
@@ -15,30 +54,10 @@ function MainMenu() {
                 selectedKeys={[useLocation().pathname]}
                 mode="horizontal"
             >
-                {isAuthenticated ? (
-                    <>
-                        <Menu.Item
-                            className="menu-item"
-                            key="/mycourses"
-                            icon={<SolutionOutlined />}
-                        >
-                            <Link to="/mycourses">My courses</Link>
-                        </Menu.Item>
-                        <Menu.Item
-                            className="menu-item"
-                            key="/courses"
-                            icon={<UnorderedListOutlined />}
-                        >
-                            <Link to="/courses">Course list</Link>
-                        </Menu.Item>
-                        <Menu.Item key="profile" className="menu-item">
-                            <Link to="/profile">Profile</Link>
-                        </Menu.Item>
-                    </>
-                ) : (
-                    <></>
-                )}
-
+                {menu}
+                {isAuthenticated ? <Menu.Item key="profile" className="menu-item" icon={<RobotOutlined />}>
+                    <Link to="/profile">Profile</Link>
+                </Menu.Item> : <></>}
                 <Menu.Item key="logout" className="menu-logout">
                     <AuthenticationButton />
                 </Menu.Item>
