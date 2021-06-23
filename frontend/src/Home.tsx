dbf4849a3867126cd79849a9270b8f3e3fb5d056
Martin Gargalovic <xgargal@fi.muni.cz>
2021-06-23 11:41:07 +0200

    login button

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -17,20 +17,18 @@ function Home() {
     const resetUserState = useResetRecoilState(userState);
 
     if (!isAuthenticated) {
-        return <a
-            onClick={() => {
-                loginWithRedirect();
-            }}
-        >
-            <Card className="grid-wrap">
-            
+        return <Card className="grid-wrap">
+            <a
+                onClick={() => {
+                    loginWithRedirect();
+                }}
+            >
                 <div className="tile">
                     <PoweroffOutlined className="tile__image" />
                     <h4 className="tile__text">Log In</h4>
                 </div>
-            
-            </Card>
-        </a>;
+            </a>
+        </Card>;
     }
     return (
         <Card className="grid-wrap">
